936906a8c267eba183fa026c45acccd24abaa989
GaidamakUA <jmazurevich@gmail.com>
2018-05-15 05:07:31 +0300

    Added animations and code rearrangement.

--- app/src/main/java/com/blogspot/androidgaidamak/sunsetapplication/MainActivity.kt
+++ app/src/main/java/com/blogspot/androidgaidamak/sunsetapplication/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.blogspot.androidgaidamak.sunsetapplication
 
 import android.Manifest
+import android.animation.AnimatorInflater
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
+import android.support.v4.widget.ImageViewCompat
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Toast
 import com.blogspot.androidgaidamak.sunsetapplication.data.SunriseSunsetResponse
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -21,7 +24,8 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.text.DateFormat
 
-
+// I was thinking about adding MVVM architecture.
+// But decided that it's too small for that yet.
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
@@ -31,16 +35,40 @@ class MainActivity : AppCompatActivity() {
     private val PLACE_PICKER_REQUEST = 1
     private lateinit var DISPLAY_TIME_FORMAT: DateFormat
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var colorAnimator: ValueAnimator
 
     private var myLocation: Location? = null
     private var activeLocation: Location? = null
     private var firstRequest = true
+    private var call: Call<SunriseSunsetResponse>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        DISPLAY_TIME_FORMAT = android.text.format.DateFormat.getTimeFormat(getApplicationContext())
+        initContextDependedConstants()
 
+        initUiListeners()
+        restoreState(savedInstanceState)
+        initMyLocation()
+
+        refreshSunriseSunset()
+    }
+
+    private fun initContextDependedConstants() {
+        if (!::DISPLAY_TIME_FORMAT.isInitialized) {
+            DISPLAY_TIME_FORMAT = android.text.format.DateFormat.getTimeFormat(getApplicationContext())
+        }
+        if (!::colorAnimator.isInitialized) {
+            colorAnimator = AnimatorInflater.loadAnimator(this, R.animator.color_animator) as ValueAnimator
+            colorAnimator.addUpdateListener {
+                ImageViewCompat.setImageTintList(sunriseIcon, ColorStateList.valueOf(colorAnimator.animatedValue as Int));
+                ImageViewCompat.setImageTintList(sunsetIcon, ColorStateList.valueOf(colorAnimator.animatedValue as Int));
+            }
+        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun initUiListeners() {
         pickLocationButton.setOnClickListener {
             val intent = PlacePicker.IntentBuilder().build(this)
             startActivityForResult(intent, PLACE_PICKER_REQUEST);
@@ -54,14 +82,13 @@ class MainActivity : AppCompatActivity() {
                 refreshSunriseSunset()
             }
         }
+    }
+
+    private fun restoreState(savedInstanceState: Bundle?) {
         if (savedInstanceState != null) {
             firstRequest = savedInstanceState.getBoolean(FIRST_REQUEST_KEY, true)
             activeLocation = savedInstanceState.getParcelable(ACTIVE_LOCATION_KEY)
         }
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        initMyLocation()
-
-        refreshSunriseSunset()
     }
 
     private fun initMyLocation() {
@@ -70,9 +97,6 @@ class MainActivity : AppCompatActivity() {
             fusedLocationClient.lastLocation
                     .addOnSuccessListener { location: Location? ->
                         myLocation = location
-                        val toastMsg = getString(R.string.location_pattern, location.toString())
-                        Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show()
-                        // Got last known location. In some rare situations this can be null.
                     }
         } else if (firstRequest) {
             firstRequest = false;
@@ -87,12 +111,15 @@ class MainActivity : AppCompatActivity() {
         outState?.putBoolean(FIRST_REQUEST_KEY, firstRequest)
     }
 
+    override fun onStop() {
+        super.onStop()
+        call?.cancel()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == PLACE_PICKER_REQUEST && resultCode == Activity.RESULT_OK) {
             val place = PlacePicker.getPlace(this, data)
-            val toastMsg = getString(R.string.place_title_pattern, place.name)
-            Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show()
-            locationTitleText.text = toastMsg
+            locationTitleText.text = getString(R.string.place_title_pattern, place.name)
 
             activeLocation = Location(LocationManager.GPS_PROVIDER);
             activeLocation?.latitude = place.latLng.latitude
@@ -116,17 +143,35 @@ class MainActivity : AppCompatActivity() {
 
     private fun refreshSunriseSunset() {
         if (activeLocation == null) return
-        ServiceLocator.sunsetApi.getSunsetSunrise(activeLocation!!.latitude, activeLocation!!.longitude)
-                .enqueue(object : Callback<SunriseSunsetResponse> {
-                    override fun onFailure(call: Call<SunriseSunsetResponse>?, t: Throwable?) {
-                        Log.d(TAG, "onResponse: call:$call, t:$t")
-                    }
+        startAnimatingIcons()
+        call?.cancel()
+        call = ServiceLocator.sunsetApi.getSunsetSunrise(activeLocation!!.latitude, activeLocation!!.longitude)
+        call?.enqueue(object : Callback<SunriseSunsetResponse> {
+            override fun onFailure(call: Call<SunriseSunsetResponse>?, t: Throwable?) {
+                animateIconsLoadingFailed()
+                Toast.makeText(this@MainActivity, R.string.loading_failed, Toast.LENGTH_LONG).show()
+            }
 
-                    override fun onResponse(call: Call<SunriseSunsetResponse>?, response: Response<SunriseSunsetResponse>?) {
-                        val results = response?.body()?.results
-                        sunriseTextView.text = DISPLAY_TIME_FORMAT.format(results?.sunrise)
-                        sunsetTextView.text = DISPLAY_TIME_FORMAT.format(results?.sunset)
-                    }
-                })
+            override fun onResponse(call: Call<SunriseSunsetResponse>?, response: Response<SunriseSunsetResponse>?) {
+                val results = response?.body()?.results
+                sunriseTextView.text = DISPLAY_TIME_FORMAT.format(results?.sunrise)
+                sunsetTextView.text = DISPLAY_TIME_FORMAT.format(results?.sunset)
+                stopAnimatingIcons()
+            }
+        })
+    }
+
+    private fun startAnimatingIcons() {
+        colorAnimator.repeatCount = -1
+        colorAnimator.start()
+    }
+
+    private fun animateIconsLoadingFailed() {
+        colorAnimator.repeatCount = 0
+        colorAnimator.reverse()
+    }
+
+    private fun stopAnimatingIcons() {
+        colorAnimator.repeatCount = 0
     }
 }
